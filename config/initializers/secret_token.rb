d8f67ef9952eaa98a73e8840a62dffe56ec4be3f
Rajesh Veliyath <rajeshn75@gmail.com>
2011-10-30 22:26:12 -0400

    initial'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rajdemo::Application.config.secret_token = '1652f22abc4aeb9b9e36c810788fdd3bb84609b65a531a4ceccc8a202cc007501512f1a854a1d3dcf76f492c5d174e2dc4d8775e8c99e2e146f7fec3f17a9efa'
